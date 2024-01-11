7f603edfc40077d6be29310db2e5f935ac423482
Quincy Thai <quincythai50@icloud.com>
2024-01-10 20:56:36 -0800

    Add lesson 03

--- lesson01/src/App.js
+++ lesson01/src/App.js
@@ -1,25 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
-  const handleNameChange = () => {
-    const names = ["Bob", "Kevin", "Dave"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello {handleNameChange()} i see that the function uses ()</p>
-        <p>{"this is javascript" + 1}</p>
-        <p>this is html</p>
-        {/* objects and booleans will Error */}
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
