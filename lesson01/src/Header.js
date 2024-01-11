522eda94b01a6d4a23852149058991084afc4933
Quincy Thai <quincythai50@icloud.com>
2024-01-10 21:14:00 -0800

    Add lesson 05

--- lesson01/src/Header.js
+++ lesson01/src/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Header = () => {
+  // const headerStyle = {
+  //   backgroundColor: "royalblue",
+  //   color: "#fff",
+  // };
   return (
     <header>
       <h1>Groceries List</h1>
